2cdb20528af3824dd100357e2598efe9a3475193
johnmorris4 <johnmorris@morrisje.com>
2019-04-04 07:18:25 -0400

    Started working on Likes

--- Migrations/20190404103150_Likes.Designer.cs
+++ Migrations/20190404103150_Likes.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DatingAppV2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingAppV2.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190404103150_Likes")]
+    partial class Likes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
